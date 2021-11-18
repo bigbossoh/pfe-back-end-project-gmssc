b664c3939dffbe6d9dffff0714de5db957a64d82
bigbossoh <michel.bossoh@outlook.fr>
2021-11-18 18:37:01 +0000

    I finished to clean the code for ours controller for swagger-ui

--- src/main/java/com/bossoh/gmsscbackend/controller/PieceController.java
+++ src/main/java/com/bossoh/gmsscbackend/controller/PieceController.java
@@ -2,40 +2,52 @@ package com.bossoh.gmsscbackend.controller;
 
 import com.bossoh.gmsscbackend.entities.Pieces;
 import com.bossoh.gmsscbackend.services.impl.PieceServiceImpl;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static com.bossoh.gmsscbackend.utils.Constants.APP_ROOT;
+
 @RestController
-@RequestMapping(path="/api/v1/piece")
+@Api(APP_ROOT+"/pieces")
 @RequiredArgsConstructor
 public class PieceController {
 
     private final PieceServiceImpl pieceService;
 
-    @GetMapping("/all")
+
+    @GetMapping(value=APP_ROOT+"/pieces/all",produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Pieces> getListeDesPieces() {
         return pieceService.listOfPieces();
     }
-    @GetMapping(path = "{IdPiece}")
+
+    @GetMapping(value=APP_ROOT+"/pieces/{IdPiece}",produces = MediaType.APPLICATION_JSON_VALUE)
     public Pieces getPieceByID(@PathVariable("IdPiece") Long IdPiece) {
         return pieceService.getPieceById(IdPiece);
     }
-    @GetMapping(path = "/getPiece/{CodePiece}")
+    @GetMapping(value=APP_ROOT+"/pieces/getPiece/{codebien}",produces = MediaType.APPLICATION_JSON_VALUE)
     public Pieces getPieceByID(@PathVariable("CodePiece") String CodePiece) {
         return pieceService.getPieceByCode(CodePiece);
     }
-    @PostMapping(path = "/savepiece")
+    @PostMapping(value =APP_ROOT+"/pieces/savepiece",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public Pieces savePiece(@RequestBody Pieces pieces) {
 
         return pieceService.savePiece(pieces);
     }
-    @DeleteMapping(path = "/delete/{idPiece}")
+    @DeleteMapping(value = APP_ROOT+"/pieces/delete/{idPiece}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public boolean deletePieceById(@PathVariable("idPiece") Long idPiece) {
         return pieceService.deletePiece(idPiece);
     }
-    @PutMapping(path="/update")
+
+    @PutMapping(value =APP_ROOT+"/pieces/update",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public Pieces updatingSociete(@RequestBody Pieces pieces) {
         return pieceService.updatePiece(pieces);
     }
--- src/main/java/com/bossoh/gmsscbackend/controller/PieceEquipementController.java
+++ src/main/java/com/bossoh/gmsscbackend/controller/PieceEquipementController.java
@@ -3,36 +3,50 @@ package com.bossoh.gmsscbackend.controller;
 import com.bossoh.gmsscbackend.entities.BienImmobilier;
 import com.bossoh.gmsscbackend.entities.PieceEquipement;
 import com.bossoh.gmsscbackend.services.impl.PieceEquipementServiceImpl;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static com.bossoh.gmsscbackend.utils.Constants.APP_ROOT;
+
 @RestController
-@RequestMapping("/api/v1/PieceEquipement")
+@Api(APP_ROOT+"/PieceEquipements")
 @RequiredArgsConstructor
 public class PieceEquipementController {
 
     private final PieceEquipementServiceImpl pieceEquipementService;
-    @GetMapping("/all")
+
+    @GetMapping(value=APP_ROOT+"/PieceEquipements/all",produces = MediaType.APPLICATION_JSON_VALUE)
     public List<PieceEquipement> getAllPieceEquipement(){
         return pieceEquipementService.listOfPieceEquipement();
     }
 
-    @GetMapping(path = "{IdPieceEqpt}")
+
+    @GetMapping(value=APP_ROOT+"/PieceEquipements/{IdPieceEqpt}",produces = MediaType.APPLICATION_JSON_VALUE)
     public PieceEquipement getPieceEquipementByID(@PathVariable("IdPieceEqpt") Long IdPieceEqpt) {
         return pieceEquipementService.getPieceEquipementyId(IdPieceEqpt);
     }
-    @PostMapping(path = "/savePieceEquipement")
+
+    @PostMapping(value =APP_ROOT+"/PieceEquipements/savePieceEquipement",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public PieceEquipement savePieceEquipement(@RequestBody  PieceEquipement pieceEquipement) {
 
         return pieceEquipementService.savePieceEquipement(pieceEquipement);
     }
-    @DeleteMapping(path = "/delete/{IdBien}")
+
+    @DeleteMapping(value = APP_ROOT+"/PieceEquipements/delete/{IdBien}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public boolean deletePieceEquipementById(@PathVariable("IdPieceEqpt") Long IdPieceEqpt) {
         return pieceEquipementService.deletePieceEquipement(IdPieceEqpt);
     }
-    @PutMapping(path="/update")
+
+    @PutMapping(value =APP_ROOT+"/PieceEquipements/update",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public PieceEquipement updatingPieceEquipement(@RequestBody PieceEquipement pieceEqpt){
         return pieceEquipementService.updatePieceEquipement(pieceEqpt);
     }
