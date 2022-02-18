84dd28afd6d47ba2dc218bd3a17ee1c4e5a24a41
bigbossoh <michel.bossoh@outlook.fr>
2022-02-18 14:41:08 +0000

    first commit

---
+++ src/main/java/com/bzada/gestimowebapi/GestimowebapiApplication.java
@@ -0,0 +1,13 @@
+package com.bzada.gestimowebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestimowebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestimowebapiApplication.class, args);
+	}
+
+}
